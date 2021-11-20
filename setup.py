a415eed63b13fccec43e441346c492fbfeea1c0b
Jay Hombal <mh4ey@virginia.edu>
2021-11-19 19:30:22 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Image recognition with Convolutional Neural Networks (CNN) is also an extremely valuable skill worthy of acquisition and practice. ',
+    author='Jay Hombal, Paul Hick, Matt Dakolios, Sudharshan Luthra',
+    license='MIT',
+)
